7c60286215cc1c310beda8c6ea34c4d7918fcc56
AymanMagdy <magdia883@gmail.com>
2020-11-24 16:30:44 +0200

    setting up the kube components, script creating the machines on gcp

---
+++ create_k8s_gcp_cluster_machines.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# The public IP
+PUBLIC_IP=$(gcloud compute addresses describe kubernetes-controller \
+--region $(gcloud config get-value compute/region) \
+--format 'value(address)')
+
+# Creating the master machine.
+gcloud compute instances create controller \
+--async \
+--boot-disk-size 200GB --can-ip-forward \
+--image-family ubuntu-1804-lts \
+--image-project ubuntu-os-cloud \
+--machine-type n2-standard-2 \
+--private-network-ip 10.240.0.10 \
+--scopes compute-rw,storage-ro,service-management,service-control,logging-write,monitoring \
+--subnet kubernetes \
+--address $PUBLIC_IP
+
+# Creating the 2 worker machines in the cluster.
+for i in 0 1; do \
+    gcloud compute instances create worker-${i} \
+    --async \
+    --boot-disk-size 200GB \
+    --can-ip-forward \
+    --image-family ubuntu-1804-lts \
+    --image-project ubuntu-os-cloud \
+    --machine-type n2-standard-2 \
+    --private-network-ip 10.240.0.2${i} \
+    --scopes compute-rw,storage-ro,service-management,service-control,logging-write,monitoring \
+    --subnet kubernetes;
+done
